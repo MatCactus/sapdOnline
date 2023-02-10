e97d8ad45d3378c9d761436a8e18497a074d683d
Xeltax <85458421+Xeltax@users.noreply.github.com>
2023-02-10 07:46:01 +0100

    [FIX] zoom to scale
    changing zoom to scale because zoom make a position changement

--- src/component/navBarButton.tsx
+++ src/component/navBarButton.tsx
@@ -5,8 +5,8 @@ export default function NavBarButton(props: { children: JSX.Element, pagePath: s
     return (
         <motion.div
             className="cursor-pointer"
-            whileTap={{ zoom: 0.95 }}
-            whileHover={{ zoom: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            whileHover={{ scale: 1.1 }}
         >
             <Link href={props.pagePath}>{props.children}</Link>
         </motion.div >
